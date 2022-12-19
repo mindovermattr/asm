d95f25a172f084c66a10cd86b4711b6324a3639c
mindovermattr <96800779+mindovermattr@users.noreply.github.com>
2022-12-19 20:34:14 +0300

    Update kurs1.C

--- kurs1.C
+++ kurs1.C
@@ -1,5 +1,6 @@
 #include <fstream>	//Для работы с файлами
 #include <iostream>	//Для работы с потоковым вводом/выводом
+#include <windows.h>
 #include <chrono>	//Для тестирование быстроты выполнения
 
 using namespace std;
@@ -8,33 +9,33 @@ using namespace std;
 //Разделяет параграфы по массиву output
 // В функцию передается указатель на массив символов из файла, 
 //Возвращает количество абзацев, или 0, если в одном из абзацев более 150 символов
-int separateParagraphs(char* temp1, int counter, int* paragraphs, char output[2000][400]) {
+int separateParagraphs(char* temp1, int counter, int* paragraphs, char output[500][400]) {
 	int counterP;
 	int i = counter - 1;
 	__asm {
 
 		xor esi, esi; //обнуление используемых регистров
 		xor edi, edi
-			xor edx, edx
+		xor edx, edx
 
 
 			;//Загруска адресов массивов
 		mov edi, output
-			mov esi, temp1;//Т.к. в функцию передаются адрес можно не загружать его при помощи lea, а взять сразу 
+		mov esi, temp1;//Т.к. в функцию передаются адрес можно не загружать его при помощи lea, а взять сразу 
 
 		mov ebx, 0
-			mov edx, -1
-			mov ecx, 0
-			xor eax, eax;
+		mov edx, -1
+		mov ecx, 0
+		xor eax, eax;
 		xor ebx, ebx
 
-			strt :
+	strt :
 		inc edx
-			inc ebx
-			cmp[esi + edx], '\n';//проверка на абзац. Если есть символ переноса строки нет, то записываем символ из массива result в output, 
-		;// иначе берем следующую ячейку массива output и складываем туда символы до символа переноса строки
+		inc ebx
+		cmp[esi + edx], '\n';//проверка на абзац. Если есть символ переноса строки нет, то записываем символ из массива result в output, 
+	;// иначе берем следующую ячейку массива output и складываем туда символы до символа переноса строки
 		jz m1
-			jnz m2
+		jnz m2
 
 
 			;//Проверка на абзац без символов,запись количества символов в массив и переход на следующую строчку двумерного массива и массива типа int
@@ -44,11 +45,12 @@ int separateParagraphs(char* temp1, int counter, int* paragraphs, char output[20
 			jz oneLetter
 			cmp ebx, 1;//Проверка на 1 символ в абзаце меньше или равен- переход
 		jbe oneLetter
-			addParagraph :
+
+	addParagraph :
 		push esi;//Ложим в стек адрес, чтобы его не потерять
-		mov esi, paragraphs
+			mov esi, paragraphs
 			dec ebx;//В ebx всегда хранится на 1 символ больше (учитывается символ переноса строки), поэтому уменьшаем его значение на 1
-		mov[esi + ecx * 4], ebx;//записываем количество символов в массив paragraphs
+			mov[esi + ecx * 4], ebx;//записываем количество символов в массив paragraphs
 		pop esi;//Возвращаем значение адреса массива символов temp1
 		xor ebx, ebx;//обнуляем счетчик количества символов	
 		inc ecx;//переходим на следующую строчку двумерного массива	
@@ -57,25 +59,25 @@ int separateParagraphs(char* temp1, int counter, int* paragraphs, char output[20
 			;//Запись элемента в двумерный массив output
 	m2:
 		push ecx;//Сохраняем значение ecx
-		imul ecx, ecx, 400;//умножаем ecx на количество символов в одной строчке двумерного массива
-		add ecx, ebx;// добавляем к элементу его позицию
-		mov al, [esi + edx]
+			imul ecx, ecx, 400;//умножаем ecx на количество символов в одной строчке двумерного массива
+			add ecx, ebx;// добавляем к элементу его позицию
+			mov al, [esi + edx]
 			mov[edi + ecx - 1], al; //записываем символ из result в output
 		pop ecx;//Возвращаем значение ecx
 		jmp checkEnd
 
 
-			oneLetter : ;//проверка на символ переноса строки, чтобы не записывать его.
+	oneLetter : ;//проверка: есть ли в абзаце символы, кроме \n, чтобы не записывать в массив абзац без символов
 		cmp ebx, 1
-			ja addParagraph;	//Если больше 1, то добавляем информацию о количестве символов
+		ja addParagraph;	//Если больше 1, то добавляем информацию о количестве символов
 		xor ebx, ebx
-			jmp checkEnd
+		jmp checkEnd
 
 
-			checkEnd : ;// проверка на конец массива
+	checkEnd : ;// проверка на конец массива
 		cmp edx, i
-			jz ending
-			jnz strt
+		jz ending
+		jnz strt
 
 			;//конец
 	ending:
@@ -85,20 +87,21 @@ int separateParagraphs(char* temp1, int counter, int* paragraphs, char output[20
 			mov[edi + ecx - 1], '\0';//замена последнего элемента массива 
 		pop ecx
 
-			mov esi, paragraphs;//Добавляется информация о количестве символов последнего абзаца
+		mov esi, paragraphs;//Добавляется информация о количестве символов последнего абзаца
 		cmp ebx, 0;//В последнем абзаце не было символов? Если да, то записываем количество абзацев в переменную, иначе - записываем количество символов и количество абзацеев 
-			jz nextStep
-			dec ebx
-			mov[esi + ecx * 4], ebx
-			inc ecx
-			nextStep :
+		jz nextStep
+		dec ebx
+		mov[esi + ecx * 4], ebx
+		inc ecx
+
+	nextStep :
 		mov counterP, ecx
 
 	}
 
-	if (counterP > 2000) {
-		cout << "В файле более 2000 абзацев. Будут взяты только первые 2000" << endl;
-		counterP = 2000;
+	if (counterP > 500) {
+		cout << "В файле более 500 абзацев. Будут взяты только первые 500" << endl;
+		counterP = 500;
 	}
 
 	for (int i = 0; i < counterP; i++)
@@ -119,7 +122,7 @@ int separateParagraphs(char* temp1, int counter, int* paragraphs, char output[20
 //Сортировка методом пузырька абзацев по количеству символов. Реализована на ассемблере
 //В Процедуру необходимо передать двумерный массив символов, количество абзацев, указатель на массив, состоящий из количества символов абзацев
 //Процедура ничего не возвращает
-void asmBubbleSort(char output[2000][400], int counterP, int* paragraphs) {
+void asmBubbleSort(char output[500][400], int counterP, int* paragraphs) {
 
 	int i = counterP - 1;
 
@@ -182,8 +185,9 @@ bool ReadFromFile(char* temp, int* a) {
 		cout << "Не удалось открыть файл" << endl;
 		return false;
 	}
+
 	int counter = 0; //счетчик символов
-	int counterN = 0;//счетчик символо переноса строки
+	int counterN = 0;//счетчик символов переноса строки
 	char ch;//символьная переменная
 
 	inputFile.seekg(0); //Считываем символы с 0 позиции файла
@@ -192,10 +196,9 @@ bool ReadFromFile(char* temp, int* a) {
 		temp[counter] = ch;
 		counterN += ch == '\n' ? 1 : 0;
 		counter++;
-		if (counter > 100000)			//Если в файле больше символов, чем может вместить массив - выход
+		if (counter > 10000)			//Если в файле больше символов, чем может вместить массив - выход
 		{
-			inputFile.close();
-			cout << "Символов больше 100000,что превышает размер массива символов, поэтому для сортировки будут использованы только первые 100000" << endl;
+			cout << "Символов больше 10000,что превышает размер массива символов, поэтому для сортировки будут использованы только первые 10000" << endl;
 			break;
 		}
 	}
@@ -216,7 +219,7 @@ bool ReadFromFile(char* temp, int* a) {
 
 //Процедура для записи в файл
 //В процедуру передается количество абзацев,двумерный массив символов, массив количества символов
-void WriteInFile(int counterParagraphs, char output[2000][400], int paragraphs[2000]) {
+void WriteInFile(int counterParagraphs, char output[500][400], int paragraphs[500]) {
 
 	ofstream outputFile("output.txt"); //запись в файл
 
@@ -237,15 +240,16 @@ int main()
 {
 	auto start_time = std::chrono::steady_clock::now();
 	int l, counter;
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian"); //Русский язык в консоли
 
-	setlocale(LC_ALL, "Russian");
-
-	char temp[100000] = { 0 }, output[2000][400] = { 0 };
+	char temp[10000] = { 0 }, output[500][400] = { 0 };
 
 	//Если не удалось прочитать из файла символы или файл пустой - выход из программы
 	if (!ReadFromFile(temp, &counter)) return 0;
 
-	int paragraphs[2000] = { 0 };
+	int paragraphs[500] = { 0 };
 
 	cout << "***************Данные из файла:****************" << endl;
 
@@ -254,7 +258,7 @@ int main()
 	switch (l)
 	{
 		case 0:
-			return 0;	//В одном из абзаце более 150 
+			return 0;	//В одном из абзаце более 400 символов 
 			break;
 		case 1:
 			cout << "В файле только 1 абзац" << endl;
@@ -271,8 +275,8 @@ int main()
 
 	auto end_time = std::chrono::steady_clock::now();
 	auto elapsed_ns = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-	std::cout << "Время работы программы:" << elapsed_ns.count() << " ms\n";
-
+	//cout << "Время работы программы:" << elapsed_ns.count() << " ms\n";
+	
 	system("pause");
 	return 1;
 }
